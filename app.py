3caca750fbe4dfe4f9e34abd2d321cd67c8a5b6c
msuarez9 <msuarez9@nd.edu>
2021-04-28 21:57:38 -0400

    change size for testing

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ app.layout = html.Div(
                         },
                         style={
                             # This controls the Map Div
-                            "height": "650px","background-color": "#f8f7f6"
+                            "height": "200px","background-color": "#f8f7f6"
                         },
                     ),style={"backgroundColor": "#f8f7f6"},
                 )
